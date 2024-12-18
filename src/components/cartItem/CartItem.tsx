5d524bbdfb3a049adbc9bdb251089bd5861fa6a3
Shzed <shakib.zeidi.dev@gmail.com>
2024-12-18 18:29:34 +0330

    ui changes

--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -2,17 +2,21 @@ import Button from "../button/Button";
 
 function CartItem() {
 	return (
-		<div className="flex justify-between flex-row-reverse mt-4 border-b pb-2 ">
-			<img
-				className="rounded w-28"
-				src="/public/car.webp"
-				alt="car-img"
-			/>
-			<div className="mr-4">
+		<div className="flex justify-between flex-row-reverse items-center mt-5 shadow p-4">
+			<div>
+				<img
+					className="rounded w-48"
+					src="/public/car.webp"
+					alt="car-img"
+				/>
+			</div>
+			<div>
 				<h2 className="text-right">عنوان محصول</h2>
+			</div>
+			<div className="mr-4">
 				<div className="mt-2">
 					<Button className="rounded mr-2 p-2" variant="danger">
-						Remove
+						حذف محصول
 					</Button>
 					<Button className="rounded p-2" variant="primary">
 						+

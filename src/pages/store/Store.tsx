76d19edd93037aa7c57b50c093c96bf6ec3546ab
Shzed <shakib.zeidi.dev@gmail.com>
2024-12-18 18:36:40 +0330

    ui changes

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -4,25 +4,23 @@ import ProductItem from "../../components/productItem/ProductItem";
 
 function Store() {
 	return (
-		<>
+		<Container>
 			<h1 className="text-right mt-5">جدیدترین محصولات</h1>
-			<Container>
-				<div className="grid grid-cols-4 gap-4 mt-4">
-					<Link to={`/product/${1}`}>
-						<ProductItem />
-					</Link>
-					<Link to={`/product/${2}`}>
-						<ProductItem />
-					</Link>
-					<Link to={`/product/${3}`}>
-						<ProductItem />
-					</Link>
-					<Link to={`/product/${4}`}>
-						<ProductItem />
-					</Link>
-				</div>
-			</Container>
-		</>
+			<div className="grid grid-cols-4 gap-4 mt-4">
+				<Link to={`/product/${1}`}>
+					<ProductItem />
+				</Link>
+				<Link to={`/product/${2}`}>
+					<ProductItem />
+				</Link>
+				<Link to={`/product/${3}`}>
+					<ProductItem />
+				</Link>
+				<Link to={`/product/${4}`}>
+					<ProductItem />
+				</Link>
+			</div>
+		</Container>
 	);
 }
 
